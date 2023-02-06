9d715bf877e4a84fe6d43209dfe0a44ed8a50687
Thiago de Oliveira <thiago.silva@ice.ufjf.br>
2023-02-06 20:25:54 -0300

    First Commit

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,164 @@
+import ActionButton from "@/shared/ActionButton";
+import HText from "@/shared/HText";
+import { BenefitType, SelectedPage } from "@/shared/types";
+import {
+    HomeModernIcon,
+    UserGroupIcon,
+    AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+import Benefit from "./Benefit";
+import BenefitsPageGraphic from '@/assets/BenefitsPageGraphic.png';
+
+
+
+
+const benefits: Array<BenefitType> = [
+    {
+        icon: <HomeModernIcon className="h-6 w-6" />,
+        title: "State of the art Facilities",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod."
+    },
+    {
+        icon: <UserGroupIcon className="h-6 w-6" />,
+        title: "100's of Diverse Classes",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod."
+    },
+    {
+        icon: <AcademicCapIcon className="h-6 w-6" />,
+        title: "Expert and Pro Trainers",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod."
+    }
+];
+
+const container = {
+    hidde: {},
+    visible: {
+        transition: {staggerChildren: 0.2}
+    }
+}
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+
+}
+
+const Benefits = ({setSelectedPage}: Props) => {
+    return (
+        <section id="benefits" className="mx-auto min-h-full w-5/6 py-20"> 
+            <motion.div
+            onViewportEnter={()=> setSelectedPage(SelectedPage.Benefits)}
+            >
+                    {/* Header*/}
+                <motion.div
+                    className="md:my-5 md:w-3/5"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay:0.2, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible:{ opacity:1, x: 0}
+                    }}
+                >
+                    <HText>MORE THAN JUST A GYM.</HText>
+                    <p className="my-5 text-sm">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus qui et aut asperiores quasi earum,
+                        ipsa nostrum iusto placeat iure dolorem dolore dignissimos, laudantium non enim laborum!
+                        Nemo, a praesentium!
+                    </p>
+                </motion.div>
+
+                
+                {/* Benefits*/}
+                <motion.div
+                    className="md:flex items-center justify-between gap-8 md:flex"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    variants={container}
+                >
+                    {benefits.map((benefit: BenefitType) => (
+                        <Benefit
+                            key={benefit.title}
+                            icon={benefit.icon}
+                            title={benefit.title}
+                            description={benefit.description}
+                            setSelectedPage={setSelectedPage}
+                        />
+                    ))}
+                </motion.div>
+
+
+                {/* Graphics and description*/}
+                <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+                    {/* Graphics*/}
+                    
+                    <img
+                        className="mx-auto"
+                        src={BenefitsPageGraphic}
+                        alt="benefits-page-graphic"
+                    />
+                    
+                    {/* Description*/}
+                    <div>
+                        {/* Title*/}
+
+                        <div className="relative">
+                            <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractWaves">
+                                <motion.div
+                                className="md:my-5 md:w-3/5"
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: true, amount: 0.5 }}
+                                transition={{ delay:0.2, duration: 0.5 }}
+                                variants={{
+                                    hidden: { opacity: 0, x: 50 },
+                                    visible:{ opacity:1, x: 0}
+                                            }}
+                                >
+                                    <HText>
+                                        MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                                        <span className="text-primary-500">FIT</span>
+                                    </HText>
+                                </motion.div>
+                            </div>
+                        </div>
+
+                        {/* Descript*/}
+                        <motion.div
+                        className="md:my-5 md:w-3/5"
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true, amount: 0.5 }}
+                            transition={{ delay:0.2, duration: 0.5 }}
+                            variants={{
+                            hidden: { opacity: 0, x: 50 },
+                            visible:{ opacity:1, x: 0}
+                    }}
+                        >
+                            <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quae nam similique, deserunt non tenetur corporis eveniet nostrum obcaecati odio.</p>
+                            
+                            <p className="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eius quod odio magnam numquam fugit excepturi molestiae, magni illum sunt! Perspiciatis animi ex ab aliquid tempore expedita, distinctio necessitatibus hic.</p>
+                        </motion.div>
+                        {/* Button*/}
+
+                        <div className="relative mt-16">
+                            <div className="before:absolute before:-bottom-20 before:right-49 before: z-[-1] before:sparkles"></div>
+
+                            <ActionButton setSelectedPage={setSelectedPage}>
+                                Join Now
+                                </ActionButton>
+                        </div>
+                    </div>
+
+                </div>
+            </motion.div>
+        </section>
+    )
+}
+
+
+export default Benefits;
